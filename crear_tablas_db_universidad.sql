ea5897fceb3937ecccb2ee62665f62f634074dea
adamulp <adamgoestoargentina@gmail.com>
2024-05-13 12:26:33 -0300

    Crear tablas y algunas sentencias jdbc

---
+++ crear_tablas_db_universidad.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-05-2024 a las 15:26:05
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `universidad`
+--
+CREATE DATABASE IF NOT EXISTS `universidad` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `universidad`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `idAlumno` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `estado` tinyint(4) DEFAULT 1  NOT NULL,
+  PRIMARY KEY (`idAlumno`),
+  UNIQUE KEY `dni` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripcion`
+--
+
+CREATE TABLE IF NOT EXISTS `inscripcion` (
+  `idInscripto` int(11) NOT NULL AUTO_INCREMENT,
+  `nota` int(11) DEFAULT NULL,
+  `idAlumno` int(11) NOT NULL,
+  `idMateria` int(11) NOT NULL,
+  PRIMARY KEY (`idInscripto`),
+  KEY `idAlumno` (`idAlumno`),
+  KEY `idMateria` (`idMateria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE IF NOT EXISTS `materia` (
+  `idMateria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `año` int(11) NOT NULL,
+  `estado` tinyint(4)  NOT NULL DEFAULT 1,
+  PRIMARY KEY (`idMateria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD CONSTRAINT `inscripcion_ibfk_1` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`),
+  ADD CONSTRAINT `inscripcion_ibfk_2` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`idMateria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- universidadEjemplo/src/universidadejemplo/UniversidadEjemplo.java
+++ universidadEjemplo/src/universidadejemplo/UniversidadEjemplo.java
@@ -1,8 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+1. Cargar el driver MariaDB.
+2. Establecer la conexión a la base de datos previamente creada.
+3. Insertar 3 alumnos
+4. Insertar 4 materias
+5. Inscribir a los 3 alumnos en 2 materias cada uno.
+6. Listar los datos de los alumnos con calificaciones superiores a 8.
+7. Desinscribir un alumno de una de la materias.
+*/
+
+
 package universidadejemplo;
+import java.sql.*;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,7 +23,112 @@ public class UniversidadEjemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            //1. Cargar el driver MariaDB.
+            Class.forName("org.mariadb.jdbc.Driver");
+        
+            //2. Establecer la conexión a la base de datos previamente creada.
+            Connection conexion = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/universidad",
+                "root",
+                "");
+        
+            //3. Insertar 3 alumnos
+            String sqlInsertAlumnos = "INSERT INTO alumno "
+                    + " (dni, apellido, nombre, fechaNacimiento, estado) "
+                    + " VALUES "
+                    + "(23423, 'Sanchez', 'Hernando', '1994-03-01', 1), "
+                    + "(43352, 'Suarez', 'Fernando', '1974-03-01', 1), "
+                    + "(23342, 'Gomez', 'Fernanda', '1998-03-01', 1)";
+            
+            PreparedStatement psInsertAlumnos = conexion.prepareStatement(
+                    sqlInsertAlumnos);
+            int filasAlumnos = psInsertAlumnos.executeUpdate();
+            if(filasAlumnos > 0) {
+                JOptionPane.showMessageDialog(null,
+                        "Alumnos agregados exitosamente");
+                System.out.println(filasAlumnos);
+            }
+        
+            //4. Insertar 4 materias
+            String sqlInsertMaterias = "INSERT INTO materia "
+                    + "(nombre, año, estado)"
+                    + " VALUES "
+                    + "('Matematica', 1, 1), "
+                    + "('Lab1', 1, 1), "
+                    + "('Web1', 1, 1), "
+                    + "('Estructuras de Datos y Algoritmos', 1, 1) ";
+            
+            PreparedStatement psInsertMaterias = conexion.prepareStatement(
+                    sqlInsertMaterias);
+            int filasMaterias = psInsertMaterias.executeUpdate();
+            if(filasMaterias > 0) {
+                JOptionPane.showMessageDialog(null,
+                        "Materias agregadas exitosamente");
+                System.out.println(filasMaterias);
+            }
+
+            //5. Inscribir a los 3 alumnos en 2 materias cada uno.
+            String sqlInscripciones = "INSERT INTO inscripcion"
+                    + " (idAlumno, idMateria) "
+                    + " VALUES "
+                    + " (1, 1), "
+                    + " (1, 2), "
+                    + " (2,2), "
+                    + " (2,4), "
+                    + " (3,3), "
+                    + " (3,1) ";
+
+            //6. Listar los datos de los alumnos con calificaciones superiores a 8.
+            String sqlNotaMayorOcho = "SELECT "
+                    + " alumno.dni AS DNI,"
+                    + " alumno.apellido AS Apellido,"
+                    + " alumno.nombre AS Nombre ,"
+                    + " alumno.fechaNacimiento AS FechaNacimiento,"
+                    + " alumo.estado AS AlumnoActivo,"
+                    + " materia.nombre AS Materia, "
+                    + " materia.año AS Año, "
+                    + " nota AS Nota "
+                    + " FROM inscripcion "
+                    + " JOIN alumno ON (inscripcion.idAlumno = alumno.idAlumno) "
+                    + " JOIN materia ON (inscipcion.idMateria = materia.idMateria) "
+                    + " WHERE nota > 8 ";
+            PreparedStatement psNotaMayorOcho = conexion.prepareStatement(
+                    sqlNotaMayorOcho);
+            ResultSet consMayorOcho = psNotaMayorOcho.executeQuery();
+            
+            while(consMayorOcho.next()){
+                System.out.println("DNI: " + consMayorOcho.getInt("DNI"));
+                System.out.println("Apellido: " + consMayorOcho.getString("Apellido"));
+                System.out.println("Nombre: " + consMayorOcho.getString("Nombre"));
+                System.out.println("FechaNacimiento: " + consMayorOcho.getDate("FechaNacimiento"));
+                System.out.println("AlumnoActivo: " + consMayorOcho.getBoolean("AlumnoActivo"));
+                System.out.println("Materia: " + consMayorOcho.getString("Materia"));
+                System.out.println("Año: " + consMayorOcho.getInt("Año"));
+                System.out.println("Nota: " + consMayorOcho.getInt("Nota"));
+                System.out.println("");
+            }
+
+            //7. Desinscribir un alumno de una de la materias.
+            String sqlDesinscribir = "DELETE FROM inscripcion "
+                    + " WHERE idAlumno = 1 "
+                    + " AND idMateria = 1 ";
+            PreparedStatement psDesinscribir = conexion.prepareStatement(sqlDesinscribir);
+            int filasBaja = psDesinscribir.executeUpdate();
+            if(filasBaja == 1) {
+                JOptionPane.showMessageDialog(null,
+                        "Inscripción dada de baja correctamente"); 
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            
+           JOptionPane.showMessageDialog(null, "Error de conexion");
+           // Error: 1049-42000: Unknown database
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar driver");
+        }
     }
     
 }
